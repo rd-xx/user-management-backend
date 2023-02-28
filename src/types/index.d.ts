b4e4c1407924024125b1e2e6eca692567a41ff03
rdx <reydux95@gmail.com>
2023-02-28 18:12:40 +0100

    fix: remove useless global declaration

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,12 +1,7 @@
-declare module 'express' {
-  export interface Request {
-    data: Record<string, unknown>;
-  }
-}
-
 declare module 'express-serve-static-core' {
   export interface Request {
     data: Record<string, unknown>;
+    session: Record<string, Record<string, string | number>>;
   }
 }
 
